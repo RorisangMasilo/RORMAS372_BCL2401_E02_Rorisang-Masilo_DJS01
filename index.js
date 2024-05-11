8ec3cc7842fc1c27015546ddfe4057fa7ce2702b
Rori <rbmasilo@gmail.com>
2024-05-11 10:19:38 +0200

    implement error constructor

--- index.js
+++ index.js
@@ -13,11 +13,15 @@ const d = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
+try {
 const d2 = d + vel * time; //calculates new distance
 const rf = fbr * time; //calculates remaining fuel
 
 // Function to calculate new velocity based on acceleration
 const calcNewVel = (vel, acc, time) => {
+  if (vel < 0 || acc < 0 || time < 0) {
+    throw new Error("Negative values not allowed for velocity, acceleration, or time.")
+  }
   return vel + acc * time;
 };
 
@@ -27,3 +31,4 @@ const vel2 = calcNewVel(acc, vel, time); //calculates new velocity based on acce
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
 console.log(`Corrected Remaining Fuel: ${rf} kg`);
+}
